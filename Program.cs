4ad1dfa5626de39e1c8b1d2e026665679b26d5ae
Christian Steentoft <chri32b0@edu.sde.dk>
2020-07-27 14:16:14 +0200

    0.1.0 Basic Functions

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace ONeuralNetwork
     {
         static void Main(string[] args)
         {
-            
+            ONeuralNetwork net = new ONeuralNetwork(new int[] { 3, 3, 3, 1 });
+            float[] values = new float[3] { 15, 25, 30 };
+            float[] result = net.FeedForward(values);
+            Console.WriteLine(result[0]);
         }
     }
 }
